3aae8f940a9d08d262bd49173f72d723e7fd571c
amy-f <frappier.amelie@gmail.com>
2016-04-27 14:48:06 -0400

    Remove token property from Projet

--- app/src/main/java/beans/Projet.java
+++ app/src/main/java/beans/Projet.java
@@ -8,12 +8,10 @@ import java.util.Date;
  */
 public class Projet {
 
-    //TODO: Est-ce qu'on garde le token? Évaluer sa pertinence.
     private int id;
     private String nom;
     private Date dateDebut;
     private Date dateFin;
-    private String token;
 
     /**
      * Constructeur sans paramètres
@@ -26,14 +24,12 @@ public class Projet {
      * @param nom le nom du projet
      * @param dateDebut la date de début du projet
      * @param dateFin la date de fin du projet
-     * @param token chaîne de caractères générée à la création du projet, sert à accéder à un projet donné via son URL
      */
-    public Projet(int id, String nom, Date dateDebut, Date dateFin, String token) {
+    public Projet(int id, String nom, Date dateDebut, Date dateFin) {
         this.id = id;
         this.nom = nom;
         this.dateDebut = dateDebut;
         this.dateFin = dateFin;
-        this.token = token;
     }
 
 
@@ -67,13 +63,5 @@ public class Projet {
 
     public void setDateFin(Date dateFin) {
         this.dateFin = dateFin;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
     }
 }
