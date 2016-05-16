7b6b5e1339ba2e96af042194bda548bfa3e2c056
cbouff <christian_bouffard@hotmail.com>
2016-05-16 15:32:56 -0400

    Ajout des preferences

--- app/src/main/java/com/example/manchotstudios/com/spring/MainActivity.java
+++ app/src/main/java/com/example/manchotstudios/com/spring/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.manchotstudios.com.spring;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.database.DataSetObserver;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -46,6 +48,7 @@ public class  MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         //Va chercher les handlers
         ProjetHandler projetHandler = new ProjetHandler(getApplicationContext());
